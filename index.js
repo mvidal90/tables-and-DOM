7e3c356a13b22a50502b96b4e35ead9ab10d5846
Marcos Vidal <devtionit@gamil.com>
2025-04-01 20:32:31 -0300

    ADD DOM example with addition acumilation

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 
+const validateQuantity = (element) => {
+    const value = element.innerText;
+    const valueNumber = parseInt(value);
+    // Resolución con styles in line
+    // if (valueNumber > 10) {
+    //     element.style.background = "#12d112"
+    // } else if (valueNumber < 10) {
+    //     element.style.background = "#bb1919"
+    //     element.style.color = "#FFF"
+    // }
+    // Resoluciòn con clases
+    // if (valueNumber > 10) {
+    //     element.className = "cell-green"
+    // } else if (valueNumber < 10) {
+    //     element.className = "cell-red"
+    // }
+    if (valueNumber !== 10) {
+        element.className = `cell-${ valueNumber > 10 ? "green" : "red" }`
+    }
+}
 
 const totalElement = document.getElementById("total")
+const data = document.getElementsByTagName("tbody")
+
+const dataRows = data[0].rows;
+
+let totalValue = 0;
+
+for (let i = 0; i < dataRows.length - 1; i++) {
+    const totalCourses = dataRows[i].lastElementChild.innerText;
+    const totalCoursesNumber = parseInt(totalCourses, 10)
+    totalValue = totalValue + totalCoursesNumber
+}
+
+totalElement.innerText = totalValue
 
 console.log("Total de cursos:", totalElement.innerText)
+
+validateQuantity(totalElement)
